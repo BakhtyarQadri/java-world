f4169bdb7200f039a4d63f0008b485201ddc6086
bakhtyar <muhammad.bakhtyar@intigral.net>
2025-07-12 18:18:55 +0500

    start practicing generic

---
+++ src/Generic.java
@@ -0,0 +1,36 @@
+public class Generic {
+
+    public static void main(String[] args) {
+
+        welcome("Hello", 456);
+
+        var stringPrinter = new Printer<String>();
+        stringPrinter.log("string printer");
+
+        var integerPrinter = new Printer<Integer>();
+        integerPrinter.log(456);
+        System.out.println(integerPrinter.getMessage(456));
+
+    }
+
+    // Generic Method
+    private static <Param1,Param2> void welcome(Param1 msg1, Param2 msg2) {
+        System.out.println(msg1 + " - " + msg2);
+    }
+
+}
+
+// Generic Class
+class Printer<T> {
+
+    public void log(T message) {
+        System.out.println(message);
+    }
+
+    public T getMessage(T message) {
+        return message;
+    }
+
+}
+
+record User (Integer id, String name) {}
